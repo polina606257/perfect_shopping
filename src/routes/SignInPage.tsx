fe8044b714943f8254ba4b665c890f63a167f8c3
polinas888 <p.shcherbinina@itransition.com>
2023-06-03 13:38:55 +0300

    logic for sign in with email and password

--- src/routes/SignInPage.tsx
+++ src/routes/SignInPage.tsx
@@ -1,4 +1,11 @@
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import {
+  getAuth,
+  signInWithPopup,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  User,
+} from "firebase/auth";
 import firebaseApp from "../utils/FirebaseSettings";
 import { getFirestore, doc, setDoc, getDoc } from "firebase/firestore";
 
@@ -9,22 +16,47 @@ const provider = new GoogleAuthProvider();
 const signInWithGoogle = async () => {
   try {
     const result = await signInWithPopup(auth, provider);
-    const userRef = doc(db, "users", result.user.uid);
-    const userSnapshot = await getDoc(userRef);
-    if (!userSnapshot.exists()) {
-      await setDoc(userRef, {
-        displayName: result.user.displayName,
-        email: result.user.email,
-        createdAt: new Date(),
-      });
-    }
+    createUserInDB(result.user);
   } catch (error: any) {
     console.log("error creating user", error.message);
   }
 };
 
+const createUserWithMailAndPassword = async (
+  email: string,
+  password: string
+) => {
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      createUserInDB(userCredential.user);
+    })
+    .catch((error) => {
+      console.log("error creating user", error.message);
+    });
+};
+
+const signInWithMailAndPassword = async (email: string, password: string) => {
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {})
+    .catch((error) => {
+      console.log("error login user", error.message);
+    });
+};
+
 const SignIn = () => {
   return <button onClick={signInWithGoogle}>Sign in with google</button>;
+};
+
+const createUserInDB = async (user: User) => {
+  const userRef = doc(db, "users", user.uid);
+  const userSnapshot = await getDoc(userRef);
+  if (!userSnapshot.exists()) {
+    await setDoc(userRef, {
+      displayName: user.displayName,
+      email: user.email,
+      createdAt: new Date(),
+    });
+  }
 };
 
 export default SignIn;
