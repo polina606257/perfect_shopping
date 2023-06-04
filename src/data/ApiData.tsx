ee82b4fd4f32456198d27efa579f91eda0ae6f00
polinas888 <p.shcherbinina@itransition.com>
2023-06-04 16:50:34 +0300

    fixes after PR was checked

---
+++ src/data/ApiData.tsx
@@ -0,0 +1,24 @@
+import Category from "../model/Caregory";
+
+export const categories = [
+  new Category(
+    "HAT",
+    "https://images.ctfassets.net/hrltx12pl8hq/4MFiRr9vFnbWzYoNSPiYXy/fca130dd40da59b06e83ee8d5789a23e/file-converter-shutterstock.jpg"
+  ),
+  new Category(
+    "JACKETS",
+    "https://images.ctfassets.net/hrltx12pl8hq/4MFiRr9vFnbWzYoNSPiYXy/fca130dd40da59b06e83ee8d5789a23e/file-converter-shutterstock.jpg"
+  ),
+  new Category(
+    "SNEAKERS",
+    "https://images.ctfassets.net/hrltx12pl8hq/4MFiRr9vFnbWzYoNSPiYXy/fca130dd40da59b06e83ee8d5789a23e/file-converter-shutterstock.jpg"
+  ),
+  new Category(
+    "WOMEN",
+    "https://images.ctfassets.net/hrltx12pl8hq/4MFiRr9vFnbWzYoNSPiYXy/fca130dd40da59b06e83ee8d5789a23e/file-converter-shutterstock.jpg"
+  ),
+  new Category(
+    "MEN",
+    "https://images.ctfassets.net/hrltx12pl8hq/4MFiRr9vFnbWzYoNSPiYXy/fca130dd40da59b06e83ee8d5789a23e/file-converter-shutterstock.jpg"
+  ),
+];
